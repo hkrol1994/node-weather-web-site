623facce2d4bead67eb2f60c7e455505f9e5d708
חיים קרול <204148290@phoenix.co.il>
2021-02-14 17:30:24 +0200

    init commit

---
+++ src/app.js
@@ -0,0 +1,90 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+
+const app = express();
+
+// Define paths for Express config
+const publicDirPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// Setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Haim Krol",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About me",
+    name: "Haim Krol",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    message: "This is some helpful text.",
+    name: "Haim Krol",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  const address = req.query.address;
+  if (!address) {
+    return res.send({
+      error: "you must provide an address",
+    });
+  }
+  geocode(address, (error, { longitude, latitude, location } = {}) => {
+    if (error) {
+      return res.send({
+        error,
+      });
+    }
+    forecast(longitude, latitude, (error, forecastData) => {
+      if (error) {
+        return res.send({
+          error,
+        });
+      }
+      res.send({
+        forecast: forecastData,
+        location,
+        address,
+      });
+    });
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Haim Krol",
+    errorMessage: "Help article not found.",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Haim Krol",
+    errorMessage: "Page not found.",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is up on port 3000");
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,28 @@
+const request = require("request");
+
+const forecast = (longitude, latitude, callback) => {
+  const url =
+    "http://api.weatherstack.com/current?access_key=c38d4cb26d91ccc27cdc502b30ac668b&query=" +
+    latitude +
+    "," +
+    longitude;
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (body.error) {
+      callback("Unabale to find location!", undefined);
+    } else {
+      callback(
+        undefined,
+        body.current.weather_descriptions[0] +
+          ". It is currently " +
+          body.current.temperature +
+          " degress out. It feels like " +
+          body.current.feelslike +
+          " degress out."
+      );
+    }
+  });
+};
+
+module.exports = forecast;
